a6dd86e0f398621fb68478c3a0d4f63a1e2c80dc
gaqg <gaqg.13@hotmail.com>
2017-12-11 18:54:15 -0700

    inicio de implementacion de MySQL

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ var users = require('./routes/users');
 
 var app = express();
 
+//database connection
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'video2015',
+  database: 'bddbus'
+})
+
+connection.connect(function(err) {
+  if (err) {
+  	console.log('unable to connect to the database :(')
+  	throw err
+  }
+  console.log('You are now connected to the database ;)')
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
