5be2f9cc4513dcde0cc462c83a684a3c98a78990
gaqg <gaqg.13@hotmail.com>
2017-12-11 18:10:06 -0700

    avance de vistas hasta apartados

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Videoturismo Streaming' });
+});
+
+router.get('/peliculas', function(req, res, next) {
+  res.render('apartados/peliculas');
+});
+router.get('/musica', function(req, res, next) {
+  res.render('apartados/musica');
+});
+router.get('/documentales', function(req, res, next) {
+  res.render('apartados/documentales');
 });
 
 module.exports = router;
